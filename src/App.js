fff1ef9577e81eccf637cd5b0b3b4a8da4314e14
obidxonn <obidxonoloviddinov1000@gmail.com>
2023-05-18 14:28:25 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import Home from './routes/home/Home'
+import Admin from './routes/admin/Admin';
 
 function App() {
   return (
     <div className="app">
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/admin' element={<Admin/>}/>
       </Routes>
     </div>
   );
