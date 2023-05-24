8909daa970a953da52eb8419443fb19af61b2b3b
obidxonn <obidxonoloviddinov1000@gmail.com>
2023-05-24 16:21:12 +0500

    update

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Home.css'
+import banner from '../../assets/banner.jpg'
+import { AiOutlineSearch } from 'react-icons/ai'
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <Link to="/admin">Tizimga Kirish</Link>
+    <div className='home'>
+      <div className="banner">
+        <img src={banner} alt="" />
+        <div className="search-container">
+          <h1>Algoritm Education <br/> Sertificate Generator</h1>
+          <div className="search-input">
+            <AiOutlineSearch/>
+            <input type="text" placeholder='AT0000'/>
+          </div>
+          <Link to="/admin">Sertifikat Yaratish</Link>
+        </div>
+      </div>
     </div>
   )
 }
